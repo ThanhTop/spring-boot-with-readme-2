5dd07d2e9d4fc0b5bb2f2b4afd34040488a841d9
thanhnm <thanhnm@gm-group.vn>
2023-08-06 18:57:40 +0700

    add homecontroller

---
+++ src/main/java/com/stackjava/springbootgitdemo/HomeController.java
@@ -0,0 +1,17 @@
+/*
+ * HomeController.java
+ *
+ * Copyright (c) 2023 Chip One Stop, Inc. All right reserved.
+ * This software is the confidential and proprietary information of Chip One Stop, Inc.
+ *
+ */
+package com.stackjava.springbootgitdemo;
+
+/**
+ * HomeController
+ *
+ * @author GM
+ */
+public class HomeController {
+
+}
